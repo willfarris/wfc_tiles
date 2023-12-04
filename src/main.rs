d51a04d239eecc9881bb21f756ad4fc8b43207a4
Will Farris <9190155+WillFarris@users.noreply.github.com>
2023-12-03 19:20:11 -0800

    cleanup, format

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Read, collections::HashMap, path::Path, time::Duration};
+use std::{collections::HashMap, io::Read, path::Path, time::Duration};
 
 use json::JsonValue;
 
@@ -21,17 +21,20 @@ trait WFCAdjacencyMap {
 impl WFCAdjacencyMap for AdjacencyMap {
     fn create(prototypes_path: &Path) -> Self {
         let mut buffer = String::new();
-        std::fs::File::open(prototypes_path).unwrap().read_to_string(&mut buffer).unwrap();
+        std::fs::File::open(prototypes_path)
+            .unwrap()
+            .read_to_string(&mut buffer)
+            .unwrap();
         let mut prototypes_json = json::parse(buffer.as_str()).unwrap();
 
         let mut prototype_map: AdjacencyMap = HashMap::new();
-            
+
         for (tile_name, tile_description) in prototypes_json.entries_mut() {
             let character_json: JsonValue = tile_description.remove("char");
             let character = character_json.as_str().unwrap().to_string();
 
             let valid_neighbors_json: JsonValue = tile_description.remove("valid_neighbors");
-            let mut valid_neighbors = HashMap::new();// [Vec<String>; 4] = [Vec::new(), Vec::new(), Vec::new(), Vec::new()];
+            let mut valid_neighbors = HashMap::new(); // [Vec<String>; 4] = [Vec::new(), Vec::new(), Vec::new(), Vec::new()];
             for (direction, valid_neighbor_list) in valid_neighbors_json.entries() {
                 valid_neighbors.insert(direction.to_string(), Vec::new());
                 if let Some(list) = valid_neighbors.get_mut(&direction.to_string()) {
@@ -81,7 +84,11 @@ trait WFCBoard {
     fn create(prototype_map: &AdjacencyMap, grid_size: isize) -> Self;
     fn get_lowest_entropy(&self) -> Vec2;
     fn is_collapsed(&self) -> bool;
-    fn propagate_collapse(&mut self, prototype_map: &AdjacencyMap, pos: &Vec2) -> Vec<(Vec2, String)>;
+    fn propagate_collapse(
+        &mut self,
+        prototype_map: &AdjacencyMap,
+        pos: &Vec2,
+    ) -> Vec<(Vec2, String)>;
     fn restore_domains(&mut self, tiles: Vec<(Vec2, String)>);
     fn is_valid_placement(&self, prototype_map: &AdjacencyMap, val: &String, pos: &Vec2) -> bool;
     fn collapse(&mut self, prototype_map: &AdjacencyMap) -> bool;
@@ -96,15 +103,14 @@ impl WFCBoard for Board {
                 board.insert([row, col], Tile::default_domain(prototype_map));
             }
         }
-        //board.remove(&[0, 0]);
-        //board.insert([0, 0], Tile::Collapsed("tl".to_string()));
-        //board.propagate_collapse(prototype_map, &[0, 0]);
+
         board
     }
 
     fn get_lowest_entropy(&self) -> Vec2 {
         let mut lowest_len = usize::MAX;
         let mut lowest_index = [0, 0];
+
         for (pos, tile) in self.iter() {
             match tile {
                 Tile::Collapsed(_) => continue,
@@ -116,22 +122,27 @@ impl WFCBoard for Board {
                 }
             }
         }
+
         lowest_index
     }
 
     fn is_collapsed(&self) -> bool {
         for (_pos, tile) in self.iter() {
-            if let Tile::Uncollapsed(_) =  tile {
+            if let Tile::Uncollapsed(_) = tile {
                 return false;
             }
         }
+
         true
     }
 
-    fn propagate_collapse(&mut self, prototype_map: &AdjacencyMap, pos: &Vec2) -> Vec<(Vec2, String)> {
-
+    fn propagate_collapse(
+        &mut self,
+        prototype_map: &AdjacencyMap,
+        pos: &Vec2,
+    ) -> Vec<(Vec2, String)> {
         let mut modified = Vec::new();
-        
+
         let val = match self.get(pos).unwrap() {
             Tile::Uncollapsed(_) => return modified,
             Tile::Collapsed(val) => val.clone(),
@@ -139,83 +150,30 @@ impl WFCBoard for Board {
         let pr = pos[0];
         let pc = pos[1];
 
-        if let Some(tile) = self.get_mut(&[pr-1, pc]) {
-            if let Tile::Uncollapsed(domain) = tile {
-                domain.retain(|adjacent_domain_element| {
-                    // retain adjacent_domain_element if prototype_map[adjacent_domain_element].valid_neighbors["right"].containts(val)
-                    if !prototype_map[adjacent_domain_element].valid_neighbors["right"].contains(&val) {
-                        modified.push(([pr-1, pc], adjacent_domain_element.clone()));
-                        return false
-                    }
-                    true
-                })
-            }
-        }
-
-        if let Some(tile) = self.get_mut(&[pr+1, pc]) {
-            if let Tile::Uncollapsed(domain) = tile {
-                domain.retain(|adjacent_domain_element| {
-                    if !prototype_map[adjacent_domain_element].valid_neighbors["left"].contains(&val) {
-                        modified.push(([pr+1, pc], adjacent_domain_element.clone()));
-                        return false
-                    }
-                    true
-                })
-            }
-        }
-
-        if let Some(tile) = self.get_mut(&[pr, pc-1]) {
-            if let Tile::Uncollapsed(domain) = tile {
-                domain.retain(|adjacent_domain_element| {
-                    if !prototype_map[adjacent_domain_element].valid_neighbors["above"].contains(&val) {
-                        modified.push(([pr, pc-1], adjacent_domain_element.clone()));
-                        return false
-                    }
-                    true
-                })
-            }
-        }
-
-        if let Some(tile) = self.get_mut(&[pr, pc+1]) {
-            if let Tile::Uncollapsed(domain) = tile {
-                domain.retain(|adjacent_domain_element| {
-                    if !prototype_map[adjacent_domain_element].valid_neighbors["below"].contains(&val) {
-                        modified.push(([pr, pc+1], adjacent_domain_element.clone()));
-                        return false
-                    }
-                    true
-                })
-            }
-        }
-
-        /*let adjacent: [(&str, Vec2); 4] = [
-            ("left", [pr-1, pc]),
-            ("right", [pr+1, pc]),
-            ("below", [pr, pc-1]),
-            ("above", [pr, pc+1]),
+        let adjacent = [
+            ("right", [pr - 1, pc]),
+            ("left", [pr + 1, pc]),
+            ("above", [pr, pc - 1]),
+            ("below", [pr, pc + 1]),
         ];
-        
-        for (label, adj_pos) in adjacent {
-            if let Some(tile) = self.get_mut(&adj_pos) {
-                match tile {
-                    Tile::Collapsed(_) => continue,
-                    Tile::Uncollapsed(domain) => {
-                        domain.retain(|tile_name| {
-                            // retain this element of the domain if prototypes[tile_name].valid_adjacent contains `val``
-                            // i.e. when a tile is collapsed, remove elements from adjacent uncollapsed domains that can't be next to it
-                            //println!("{} - {}, {:?}", tile_name, label, prototype_map[tile_name].valid_neighbors.get(label));
-                            if prototype_map[tile_name].valid_neighbors.get(label).unwrap().contains(&val) {
-                                true
-                            } else {
-                                modified.push((adj_pos.clone(), tile_name.clone()));
-                                //println!("{:?}", adj_pos);
-                                false
-                            }
-                        });
-                    }
+
+        for (adjacent_name, adjacent_pos) in adjacent {
+            if let Some(tile) = self.get_mut(&adjacent_pos) {
+                if let Tile::Uncollapsed(domain) = tile {
+                    domain.retain(|adjacent_domain_element| {
+                        // retain adjacent_domain_element if prototype_map[adjacent_domain_element].valid_neighbors["right"].containts(val)
+                        if !prototype_map[adjacent_domain_element].valid_neighbors[adjacent_name]
+                            .contains(&val)
+                        {
+                            modified.push((adjacent_pos, adjacent_domain_element.clone()));
+                            return false;
+                        }
+                        true
+                    })
                 }
             }
-        }*/
+        }
+
         modified
     }
 
@@ -236,34 +194,21 @@ impl WFCBoard for Board {
         let pr = pos[0];
         let pc = pos[1];
 
-        if let Some(tile) = self.get(&[pr-1, pc]) {
-            if let Tile::Collapsed(adj_val) = tile {
-                if !prototype_map.get(adj_val).unwrap().valid_neighbors["right"].contains(val) {
-                    return false;
-                }
-            }
-        }
+        let adjacent = [
+            ("right", [pr - 1, pc]),
+            ("left", [pr + 1, pc]),
+            ("above", [pr, pc - 1]),
+            ("below", [pr, pc + 1]),
+        ];
 
-        if let Some(tile) = self.get(&[pr+1, pc]) {
-            if let Tile::Collapsed(adj_val) = tile {
-                if !prototype_map.get(adj_val).unwrap().valid_neighbors["left"].contains(val) {
-                    return false;
-                }
-            }
-        }
-
-        if let Some(tile) = self.get(&[pr, pc-1]) {
-            if let Tile::Collapsed(adj_val) = tile {
-                if !prototype_map.get(adj_val).unwrap().valid_neighbors["above"].contains(val) {
-                    return false;
-                }
-            }
-        }
-
-        if let Some(tile) = self.get(&[pr, pc+1]) {
-            if let Tile::Collapsed(adj_val) = tile {
-                if !prototype_map.get(adj_val).unwrap().valid_neighbors["below"].contains(val) {
-                    return false;
+        for (adjacent_name, adjacent_pos) in adjacent {
+            if let Some(tile) = self.get(&adjacent_pos) {
+                if let Tile::Collapsed(adjacent_val) = tile {
+                    if !prototype_map.get(adjacent_val).unwrap().valid_neighbors[adjacent_name]
+                        .contains(val)
+                    {
+                        return false;
+                    }
                 }
             }
         }
@@ -282,7 +227,7 @@ impl WFCBoard for Board {
             Tile::Collapsed(_) => panic!("lowest entropy shouldn't be collapsed"),
             Tile::Uncollapsed(domain) => domain.clone(),
         };
-        
+
         possible_tiles.shuffle(&mut thread_rng());
 
         for possible_tile in possible_tiles.iter() {
@@ -299,9 +244,9 @@ impl WFCBoard for Board {
                 if self.collapse(prototype_map) {
                     return true;
                 }
-                self.restore_domains(modified);
                 self.remove(&pos);
                 self.insert(pos, Tile::Uncollapsed(saved_domain));
+                self.restore_domains(modified);
             }
         }
 
@@ -313,7 +258,7 @@ impl WFCBoard for Board {
         print!("\x1B[2J\x1B[1;1H");
         for c in 0..grid_size {
             for r in 0..grid_size {
-                let pos = [r, grid_size-c];
+                let pos = [r, grid_size - c];
                 if let Some(tile) = self.get(&pos) {
                     match tile {
                         Tile::Collapsed(tile_name) => {
@@ -329,12 +274,10 @@ impl WFCBoard for Board {
 }
 
 fn main() {
-    
     let prototype_map = AdjacencyMap::create(Path::new("prototypes.json"));
     let mut board = Board::create(&prototype_map, GRID_SIZE);
 
     board.collapse(&prototype_map);
 
     board.print(&prototype_map, GRID_SIZE);
-
 }
